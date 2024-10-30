27a800e5cf964095a80401c98fa0604ef51d959f
Samuel1204-lab <samukarodrigues07@gmail.com>
2024-10-30 16:54:38 -0400

    task0001

---
+++ exemplovariavel.php
@@ -0,0 +1,9 @@
+<?php
+    $nome  = "Samuel";
+    $sobrenome = "Rodrigues";
+    $idade = "17";
+    $profissao = "Operador de Loja";
+    
+    echo "Oi $profissao $nome $sobrenome, <br>";
+    echo "Você possui $idade anos.";
+?>
